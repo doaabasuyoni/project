b3bd630afb18c9a2908e858ef29d731da1a562c2
doaabasuyoni <111387939+doaabasuyoni@users.noreply.github.com>
2022-12-12 19:57:31 +0300

    Update connected.php

--- connected/connected.php
+++ connected/connected.php
@@ -3,7 +3,7 @@ $server="database-2.c0r0ncclxwvt.us-east-1.rds.amazonaws.com";
 $user = "admin";
 $pass="asdf##1234";
 $db="image";
-$conn = mysqli_connect($host,$user,$pass,$db);
+$conn = mysqli_connect($server,$user,$pass,$db);
 if(!$conn){
 echo "<script>alert('error connected');</script>";
 echo "error".mysqli_connect_errno();
